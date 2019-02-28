830390015fc27a17f0ce6afe2ac24eed871dbcdd
Felipe Cortez <felipecortezfi@gmail.com>
2019-02-27 21:59:55 -0300

    Add Ring and Compojure

--- gen/project.clj
+++ gen/project.clj
@@ -4,5 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ring "1.5.0"]
+                 [compojure "1.5.1"]
                  [hiccup "1.0.5"]]
   :repl-options {:init-ns gen.core})
