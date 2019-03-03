224da2341f98ab74df6ac77b0ef2a344ee7e104f
Felipe Cortez <felipecortezfi@gmail.com>
2019-03-03 11:07:08 -0300

    Change HTML->PDF rendering backend to puppeteer/chromium
    Renders faster and uses Chrome so no need to worry about unsupported CSS
    features

--- cv_to_pdf.sh
+++ cv_to_pdf.sh
@@ -1,9 +1,3 @@
 rm -rf build
 mkdir build
-cd build
-
-wget 127.0.0.1:3000 -O cv.html
-
-cp -r ../gen/resources/public/* .
-weasyprint cv.html cv.pdf
-ls | grep -v cv.pdf | xargs rm
+node pdf_gen.js
